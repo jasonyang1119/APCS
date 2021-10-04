7e80fa74004236f1e36e295a697dc01787afcf84
jasonyang1119 <90873779+jasonyang1119@users.noreply.github.com>
2021-10-04 19:46:11 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,5 +1,5 @@
 public class Greet {
-    public static void main( String[] args ){
+    public static void main(String[] args ){
         System.out.println( BigSib.greet("Bob") );
         System.out.println( BigSib.greet("Pinocchio") );
     }
