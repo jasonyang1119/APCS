75838c6cdcbcd9e84a06bc5601cc17c8145bd0db
jasonyang1119 <90873779+jasonyang1119@users.noreply.github.com>
2021-10-03 21:00:30 -0400

    duckynames

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -17,5 +17,6 @@ QUESTIONS: Is there a difference between calling a class from a different file a
 public class BigSib {
 	public static void greet(String name){
 		System.out.println("Why, hello there, " + name + "How do you do?");
+	}
 }
-}
+
