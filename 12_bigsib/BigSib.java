7a24f692003e2c1db97b2daae75022dc7ed1ac7a
jasonyang1119 <90873779+jasonyang1119@users.noreply.github.com>
2021-10-05 23:57:23 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,3 +1,18 @@
+/*
+BJLT + Tommy + Bob
+APCS
+HW12: OnElderIndividualityandtheEliminationofRadioFuzz
+2021-10-05
+*/
+
+/*
+DISCO: You can have an instance variable is it is in a non static class
+QCC: Why can't we just use a basic system.out.print(" ")?Would seem much simpler.
+*/
+
+
+
+
 public class BigSib {
 	public String helloMsg;
 
