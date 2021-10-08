51751c88baae428ba93fab965b1a7840dfeb76ef
jasonyang1119 <90873779+jasonyang1119@users.noreply.github.com>
2021-10-07 23:41:42 -0400

    Update BigSib.java

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -5,16 +5,11 @@ HW14: Customize Your Creation
 2021-10-07
 */
 
-
-
 public class BigSib {
 	public String helloMsg;
 	
-	public BigSib(){
-		helloMsg= "Wdo";
-	}
 	public BigSib(String input){
-		helloMsg=input;
+		helloMsg= "Word up" 
 	}
 	public String greet(String input){
 		return helloMsg  + " " + input ;
