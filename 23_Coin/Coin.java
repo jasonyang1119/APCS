ac556130aefe57103728b2a815e71e0934eb59a6
jasonyang1119 <90873779+jasonyang1119@users.noreply.github.com>
2021-10-23 21:07:07 -0400

    Update Coin.java

--- 23_Coin/Coin.java
+++ 23_Coin/Coin.java
@@ -1,6 +1,12 @@
-/***
- * class Coin by Clyde "Thluffy" Sinclair SKELETON
- ***/
+/* DJ - Jason Yang, David Chen
+   APCS
+   HW23 -- What does Equality Look Like?
+   2021-10-23
+   time spent: 1.0 hours
+   DISCO: Printing the object name automatically invokes the toString() method of the class.
+          Math.random returns a random value between 0.0 and 1.0
+   QCC: N/A
+ */
 
 public class Coin {
 
