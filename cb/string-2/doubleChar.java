cf1fefd3e87f9ebb1ab81a4e0e58addcedd4c055
jasonyang1119 <90873779+jasonyang1119@users.noreply.github.com>
2021-10-30 17:32:07 -0400

    main

--- cb/string-2/doubleChar.java
+++ cb/string-2/doubleChar.java
@@ -1,7 +1,19 @@
+/*Given a string, return a string where for every char in the original, there are two chars.*/
+
+public static void main(String[] args) {
+        doubleChar test = new doubleChar();
+        System.out.println(test.doubleChar("The"));
+        System.out.println(test.doubleChar("AAbb"));
+        System.out.println(test.doubleChar("Hi-There"));
+        System.out.println(test.doubleChar("Word!"));
+        System.out.println(test.doubleChar("!!"));
+    }
+
 public String doubleChar(String str) {
   String first = "";
   for (int i = 0; i<str.length(); i++){
     first = first + str.charAt(i) + str.charAt(i);
   }
   return first;
+}
 }
