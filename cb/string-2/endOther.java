8b3660b1b886dc7fc95f139662f2141eb226d9de
jasonyang1119 <90873779+jasonyang1119@users.noreply.github.com>
2021-10-30 17:33:59 -0400

    main

--- cb/string-2/endOther.java
+++ cb/string-2/endOther.java
@@ -1,3 +1,16 @@
+
+/*Given two strings, return true if either of the strings appears at the very end of the other string, ignoring upper/lower case differences 
+(in other words, the computation should not be "case sensitive"). Note: str.toLowerCase() returns the lowercase version of a string.*/
+
+public class endOther {
+    public static void main(String[] args) {
+        endOther test = new endOther();
+        System.out.println(test.endOther("Hiabc", "abc"));
+        System.out.println(test.endOther("AbC", "HiaBc"));
+        System.out.println(test.endOther("abc", "abXabc"));
+    }
+
+
 public boolean endOther(String a, String b) {
   String alower = a.toLowerCase();
   String blower = b.toLowerCase();
@@ -10,3 +23,4 @@ public boolean endOther(String a, String b) {
     return alower.equals(blower.substring(blength-alength,blength));
   }
   }
+}
